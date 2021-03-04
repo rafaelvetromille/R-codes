1e75246aae3c8b9b73ce656f3e4dd77d9e665855
rafaelvetromille1@gmail.com <rafaelvetromille1@gmail.com>
2021-03-04 13:44:11 -0300

    003

--- 003_Usando_a_Linha_De_Comando_Git/003_usando_linha_de_comando_git.R
+++ 003_Usando_a_Linha_De_Comando_Git/003_usando_linha_de_comando_git.R
@@ -1,14 +1,14 @@
 # R CODES ----
 # 👉 CODE 003: Fazendo o commit e push pela linha de comando ----
 
-# -- Atalho para "pular" para o terminal
+# -- Atalho para "pular" para o terminal e outros
 
-# New terminal: Alt + Shift + R
-# Back to console: Ctrl + 2
-# Send current line/selection to terminal: Ctrl + Alt + Enter
+New terminal: Alt + Shift + R
+Back to console: Ctrl + 2
+Send current line/selection to terminal: Ctrl + Alt + Enter
 
 # -- Códigos para adicionar
 
-# git add -A
-# git commit -m "comentário a desejar"
-# git push
+git add -A
+git commit -m "comentário a desejar"
+git push
